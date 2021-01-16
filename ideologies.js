341d860dfe391322b16d26b7562fd683b5fb60e6
8valueshu <77540831+8valueshu@users.noreply.github.com>
2021-01-16 16:29:25 +0100

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,470 @@
+ideologies = [
+    {
+        "name": "Anarcho-kommunizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Libertárius kommunizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Trockizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Marxizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 80
+        }
+    },
+    {
+        "name": "De Leonizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Leninizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Sztálinizmus/Maoizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Vallásos kommunizmus",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Államkommunizmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Teokratikus szocializmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Vallásos szocializmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 70,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Demokratikus szocializmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Forradalmi szocializmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Libertárius szocializmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Anarcho-szindikalizmus",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Baloldali populizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Teokratikus disztributizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Disztributizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Szociális liberalizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Keresztény demokrácia",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Szociális demokrácia",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Progresszivizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Anarcho-mutualizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Nemzeti totalitáriánizmus",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Globális totalitáriánizmus",
+        "stats": {
+            "econ": 50,
+            "dipl": 80,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Technokrácia",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Centrizmus",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Liberalizmus",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Vallásos anarchizmus",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Jobboldali populizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Mérsékelt konzervativizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Reakciós",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Szociális libertarianizmus",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Libertarianizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 60,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Anarcho-egoizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Nácizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Autokrácia",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Fasizmus",
+        "stats": {
+            "econ": 40,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Kapitalista fasizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Konzervativizmus",
+        "stats": {
+            "econ": 30,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Neo-liberalizmus",
+        "stats": {
+            "econ": 30,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Klasszikus liberalizmus",
+        "stats": {
+            "econ": 30,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Autoriter kapitalizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 20,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Államkapitalizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Neo-konzervativizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Fundamentalizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Libertárius kapitalizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Piaci anarchizmus",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Objektivizmus",
+        "stats": {
+            "econ": 10,
+            "dipl": 50,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Totalitárius kapitalizmus",
+        "stats": {
+            "econ": 0,
+            "dipl": 30,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Ultra-kapitalizmus",
+        "stats": {
+            "econ": 0,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Anarcho-kapitalizmus",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,632 @@
+questions = [
+    {
+        "question": "A nagyvállalatok általi elnyomás nagyobb probléma az államigazgatás elnyomásánál.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Szükség van arra, hogy egy kormány beleavatkozzon a gazdaságba a fogyasztók védelme érdekében.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Minél szabadabb a piac, annál szabadabbak az emberek.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A költségvetés kiegyensúlyozottsága fontosabb minden polgár jóléténél.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A piaci alapon finanszírozott kutatásokkal szemben a közfinanszírozott kutatásokkal jobban járnak az emberek.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A nemzetközi kereskedelem vámolása fontos a helyi termelés ösztönzése érdekében.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A javak elosztása mindenkitől a képességeinek megfelelően, mindenkinek a szüksége szerint történjék.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Jobb lenne ha a szociális programokat felszámolnák, hogy előtérbe kerülhessenek a magánalapítványok.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Jobban meg kéne adóztatni a gazdagokat, hogy a szegényeknek jobb körülményeket tudjunk biztosítani.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az örökség jogos vagyoni forrás.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Az alapvető közszolgáltatásoknak mint az utaknak vagy a villanynak az állam tulajdonában van a helye.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kormány közbeavatkozása fenyegetés a gazdaságra nézve.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Azok akik meg tudják maguknak engedni jogosultak magasabb szintű egészségügyi ellátásra.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A minőségi oktatás minden ember alapvető joga.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A megtermelt javak felett azon munkások rendelkezzenek akik megtermelték azokat.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az Egyesült Nemzetek Szervezetét fel kell számolni.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A katonai beavatkozás gyakran szükséges népünk megvédése érdekében.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Támogatom az olyan regionális uniók működését mint az Európai Unió.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Fontos, hogy fenntartsuk népünk szuverenitását.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Egy egyesült világkormány az emberiség előnyére válna.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Fontosabb a béke megőrzése mint a hatalom kiterjesztése.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Nem szükséges megindokolni, hogy miért indítunk háborúkat más országokkal szemben.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A katonai finanszírozás pénzpocséklás.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A nemzetközi segély pénzpocséklás.",
+        "effect": {
+            "econ": -5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az én nemzetem nagyszerű.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kutatásokat nemzetközi szinten kell elvégezni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A kormányoknak felelőseknek kellene lennie a nemzetközi közösség felé.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Még ha egy tekintélyelvű kormánnyal szemben is lépünk fel, az erőszak elfogadhatatlan.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Vallásom értékeit minél szélesebb körben kell terjeszteni.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Nemzetünk értékeit minél szélesebb körben kell terjeszteni.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Fontos a törvény és a rend fenntartása.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "A lakosság általában rossz döntéseket hoz.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az eutanáziát legálissá kellene tenni.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Némely polgári szabadságjogunk feláldozása szükséges hogy védve maradjunk a terrorizmustól.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kormányzati megfigyelés szükséges a modern világban.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Az államigazgatás már önmaga létezésében is fenyegeti szabadságunkat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Politikai véleménytől függetlenül fontos hogy saját országunk oldalán álljunk.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Semmilyen hatalom nem maradhat megkérdőjelezhetetlenül.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "A hierarchikus társadalom létezése ideális.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Fontos hogy a kormány még akkor is kövesse a többség véleményét ha az esetleg helytelen.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Minél erősebb a vezetés, annál jobb.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A demokrácia sokkal több mint döntéshozói folyamat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A környezetvédelmi előírások létfontosságúak.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Egy jobb világ fog származni az automatizálásból, valamint a tudomány és a technológia fejlődéséből.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A gyerekeket vallási és hagyományos értékekre kell tanítani.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A hagyományok önmagukban nem számítanak értéknek.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A vallásnak szerepet kell játszania a kormányzásban.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A templomokat úgy kellene megadóztatni ahogyan más intézményeket is.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A klímaváltozás a jelenlegi legnagyobb fenyegetést jelenti az életünkre.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Fontos, hogy együttes erővel fogjunk össze a klímaváltozás elleni harcban.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A társadalom jobb volt évekkel ezelőtt mint most.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Fontos, hogy fenntartsuk múltunk hagyományait.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Fontos hosszútávon gondolkodnunk, akár a saját élettartamunkon tovább.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Az észszerűség fontosabb mint a kultúránk fenntartása.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A droghasználatot legalizálni vagy legalább dekriminalizálni kellene.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "Az azonos neműek házasságát legalizálni kellene.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Egy kultúra sem felsőrendűbb a másiknál.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "A házasságon kívüli szex erkölcstelen.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Ha egyáltalán befogadjuk a migránsokat, fontos asszimilálnunk őket kultúránkba.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Az abortuszt be kell tiltani legtöbb, vagy minden esetben.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A fegyvertartást be kell tiltani azoknak akiknek nincs rá nyomós okuk.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Támogatom az egyfizetős, univerzális egészségügyi ellátást.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A prostitúció legyen illegális.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A családi értékek fenntartása kulcsfontosságú.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Minden áron a haladást üldözni veszélyes.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "A génmódosítás egy hasznos eszköz, még az embereken is.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Meg kell nyitnunk a határokat a bevándorlás számára.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A kormányoknak legalább annyira aggódniuk kellene a külföldiekért mint saját polgáraikért.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Minden embert - kultúrától vagy szexualitástól függetlenül - egyenlőnek kell kezelni.",
+        "effect": {
+            "econ": 10,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Fontos, hogy közösségem célkitűzéseit terjesszem mindenek felett.",
+        "effect": {
+            "econ": -10,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -10
+        }
+    }
+];
